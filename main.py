7a7cef5e4626262b782a7086a32c656ea111b2c2
James Carney <James@semperclassics.com>
2023-02-01 06:47:12 -0500

    need to make this work on both linux and window...

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     ############################################################
 
     hip_number_re = re.compile(r"(HIP NUMBER:)(?P<hip>\d{1,4})")
-    source_file_re = re.compile(r"[A-Z]{2}\d{6}\.TXT")
+    source_file_re = re.compile(r"[A-Z]{2}\d{6}\.\w{3}")
     one_dam_re = re.compile(r'1st dam\n')
     three_dam_re = re.compile(r'3rd dam\n')
     sex_sire_re = re.compile(r'\(\d{4}\s')
@@ -30,16 +30,17 @@ def main():
     # Paths
     ############################################################
 
-    p_source = os.getcwd() + r"\source"
-    p_original = p_source + r"\original"
-    p_update = p_source + r"\update"
-    p_report =  os.getcwd() + r"\report"
+    p_source = os.getcwd() + r"/source"
+    p_original = p_source + r"/original"
+    p_update = p_source + r"/update"
+    p_report =  os.getcwd() + r"/report"
 
     ############################################################
     # Initializing difflib
     ############################################################
 
     # d = difflib.Differ()
+
 
     ############################################################
     # Functions
@@ -57,14 +58,10 @@ def main():
 
     def get_meta_data():
         '''Gets the hip number and source file name from the list of lines, ignores hips with withdrawn status'''
-        # global hip_list
-        # global source_list
         hip_list = []
         source_list = []
-
         for line in f1:
             if hip_number_re.search(line):
-                # hip_number_re.search('hip')
                 if "WITHDRAWN" in line:
                     pass
                 else:
@@ -73,28 +70,47 @@ def main():
                 source_list.append(source_file_re.search(line).group())
             else:
                 pass
+        source_list = [x.replace('.TXT', '') for x in source_list]
         return hip_list, source_list
 
-    hip_list = get_meta_data()[0]
-    source_list = get_meta_data()[1]
 
-
-    def get_line_number(folder, re_string):
-        '''Cleans the files and puts each horse on its own line'''
+    def seperate_lines(folder):
+        '''Puts each horse on its own line'''
         i = 0
-        line_number = []
-        for f in glob.glob(f'{folder}/*_clean.txt'):
-            os.remove(f)
+        # for f in glob.glob(f'{folder}/*_clean.txt'):
+        #     os.remove(f)
         for horse in zip(hip_list, source_list):
-            with open(f"{folder}/{source_list[i]}", "r") as f: #, open(f"{folder}/{source_list[i].split('.')[0]}_clean.txt", "w") as f2:
+            i = 0
+            with open(f"{folder}/{source_list[i]}.txt", "r") as f, open(f"{folder}/{source_list[i]}_string.txt", "w") as f2: #.split('.')[0]
+                list_sex_sire = get_line_number(f"{folder}/" + source_list[i], sex_sire_re)
+                print(list_sex_sire)
                 flines = f.readlines()
-                num = 0
                 for line in flines:
-                    if re_string.search(line) != None:
-                        line_number.append(num)
-                        # f2.writelines(line)
-                    num += 1
-            i += 1
+                    print("line", i)
+                    i += 1
+                
+                # l = 0
+            #     for line in flines:
+            #         print(l)
+            #         # print(line)
+            #         f2.writelines(line)
+            #         l += 1
+            # i += 1
+
+
+    def get_line_number(file_name, re_string):
+        '''Gets the line number of a specific regex in a file.'''
+        line_number = []
+        f = open(f"{file_name}.txt", 'r')
+        lines = f.readlines()
+        num = 0
+        for line in lines:
+            if re_string.search(line):
+                # line_number = line_number.append(num)
+                # print(num)
+                line_number.append(num)
+                # i += 1
+            num += 1
         return line_number
 
 
@@ -105,20 +121,16 @@ def main():
         files = glob.glob(f'{report}/*')
         for f in files:
             os.remove(f)
-
-        # for f in files:
-        #     os.remove(f)
-        # print(os.getcwd())
         # hip_list = ["{:06d}".format(int(x)) for x in hip_list]
         for hip in zip(original, update):
             # Need to add a check to see if the file exists in the directory
-            if os.path.isfile(f"source/original/{source_list[i]}") and os.path.isfile(f"source/update/PH{hip_list[i].zfill(6)}.TXT"):
+            if os.path.isfile(f"source/original/{source_list[i]}") and os.path.isfile(f"source/update/PH{hip_list[i].zfill(6)}.txt"):
                 f = open(f"{p_original}/{source_list[i]}", "r")
-                f2 = open(f"{p_update}/PH{hip_list[i].zfill(6)}.TXT", "r")
+                f2 = open(f"{p_update}/PH{hip_list[i].zfill(6)}.txt", "r")
                 flines = f.readlines()
                 flines2 = f2.readlines()
-                diff = difflib.unified_diff(flines, flines2, fromfile=f"{source_list[i]}", tofile=f"PH{hip_list[i].zfill(6)}.TXT")
-                diffh = difflib.HtmlDiff().make_file(flines, flines2, f"{source_list[i]}", f"PH{hip_list[i].zfill(6)}.TXT")
+                diff = difflib.unified_diff(flines, flines2, fromfile=f"{source_list[i]}", tofile=f"PH{hip_list[i].zfill(6)}.txt")
+                diffh = difflib.HtmlDiff().make_file(flines, flines2, f"{source_list[i]}", f"PH{hip_list[i].zfill(6)}.txt")
                 # print(''.join(diff))
                 with open(f"{p_report}/{hip_list[i]}.html", "w") as f:
                     print("Creating report for HIP", hip_list[i])
@@ -137,23 +149,23 @@ def main():
         return list_difference
 
 
-    def write_strings(line, length, old_file, new_file):
-        old_f = open(old_file, 'r')
-        new_f = open(new_file, 'w')
-        old_l = old_f.readlines()
-        lines = old_f.readlines()
-        n = 0
-        for i, v in enumerate(line_number, start=0):
-            if i < len(list_difference):
-                group = old_l[v:v+list_difference[i]]
-            else:
-                group = old_l[v:v+list_difference[i-1]]
-            string_group = ''.join(group)
-            string_group = string_group.replace("\n", "")
-            if "2nd dam" in string_group:
-                string_group = string_group[:string_group.find("2nd dam")]
-            new_f.writelines(string_group)
-            new_f.write("\n")
+    # def write_strings(line, length, old_file, new_file):
+    #     old_f = open(old_file, 'r')
+    #     new_f = open(new_file, 'w')
+    #     old_l = old_f.readlines()
+    #     lines = old_f.readlines()
+    #     n = 0
+    #     for i, v in enumerate(line_number, start=0):
+    #         if i < len(list_difference):
+    #             group = old_l[v:v+list_difference[i]]
+    #         else:
+    #             group = old_l[v:v+list_difference[i-1]]
+    #         string_group = ''.join(group)
+    #         string_group = string_group.replace("\n", "")
+    #         if "2nd dam" in string_group:
+    #             string_group = string_group[:string_group.find("2nd dam")]
+    #         new_f.writelines(string_group)
+    #         new_f.write("\n")
 
 
 
@@ -162,31 +174,40 @@ def main():
 # Main part of the program
 ##########################################################################################
 
+    # Read the files
     print("reading files...")
     read_file_lines(f"{file_name}") #file_name is from argv
     print("Done reading files")
+    
+    # Get source file names and hip numbers
     print("Getting meta data...")
-    get_meta_data()
-    print(hip_list)
-    print(source_list)
+    # get_meta_data()
+    hip_list = get_meta_data()[0]
+    source_list = get_meta_data()[1]
+    print("Hip list from step 1:\n", hip_list)
+    print("Source list from step 1:\n", source_list)
     print("Done with meta data")
-    ## Need to clean file first
-    print("getting line numbers of 1st dam...")
-    line_original_1d = get_line_number(p_original, one_dam_re)
-    print(line_original_1d)
-    print("Getting line numbers of 3rd dam...")
-    line_original_3d = get_line_number(p_original, three_dam_re)
-    print(line_original_3d)
-    line_race_record = get_line_number(p_original, race_record_re)
-    print(line_race_record)
-    print("Creating report...")
-    print(os.getcwd())
-    print(p_report)
-    print(p_original)
-    print(p_update)
-    diff_report(source_list, hip_list, p_report)
-    print("Done with report")
-    # print(hip_list)
+    seperate_lines(p_original)
+    # test = get_line_number('FM1-586', one_dam_re)
+    # print(test)
+    ## Find locations of the 1st and 3rd dam, Race Record, and then sex and sire 
+    # print("getting line numbers of 1st dam...")
+    # line_original_1d = get_line_number(p_original, one_dam_re)
+    # print(line_original_1d)
+    # print("Getting line numbers of 3rd dam...")
+    # line_original_3d = get_line_number(p_original, three_dam_re)
+    # print(line_original_3d)
+    # line_race_record = get_line_number(p_original, race_record_re)
+    # print(line_race_record)
+
+
+    # sex_sire_list = get_line_number(p_original, sex_sire_re)
+    # print("Sex Sire line list:\n", sex_sire_list)
+    # print("Creating report...")
+    # print("Current source list:\n", source_list)
+    # print(source_list)
+    # print("Current hip list:\n", hip_list)
+    # diff_report(source_list, hip_list, p_report)
 
 
 if __name__ == "__main__":
